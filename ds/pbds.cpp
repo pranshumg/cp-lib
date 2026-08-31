755eca326890e67291ee81763395ae6c020751b3
pranshumg <pranshumg@gmail.com>
2026-08-31 20:16:09 +0530

    formatted

--- ds/pbds.cpp
+++ ds/pbds.cpp
@@ -3,9 +3,9 @@
 
 template <typename K, typename V, typename Comp = std::less<K>>
 using ordered_map = __gnu_pbds::tree<
-    K, V, Comp,
-    __gnu_pbds::rb_tree_tag,
-    __gnu_pbds::tree_order_statistics_node_update
+  K, V, Comp,
+  __gnu_pbds::rb_tree_tag,
+  __gnu_pbds::tree_order_statistics_node_update
 >;
 
 template <typename K, typename Comp = std::less<K>>
